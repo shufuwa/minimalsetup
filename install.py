c9f1d85e27472f726b927547f6f426a8db5897d3
shufuwa <shugobatista@protonmail.com>
2022-05-26 22:36:48 +0530

    fixes

--- install.py
+++ install.py
@@ -7,26 +7,26 @@ home = os.environ['HOME']
 def install_prog(pm):
     if pm == "apt":
         packages = "git build-essential htop tree tmux wget curl"
-        subprocess.run(["sudo", pm, "update"])
-        subprocess.run(["sudo", pm, "upgrade"])
-        subprocess.run(["sudo", pm, "install", packages])
-        subprocess.run(["mkdir", "-p", f"{home}/.local/bin"])
-        subprocess.run(["wget", "https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage", "-O", f"{home}/.local/bin/nvim"])
+        subprocess.run(f"sudo {pm} update", shell=True)
+        subprocess.run(f"sudo {pm} upgrade", shell=True)
+        subprocess.run(f"sudo {pm} install {packages}", shell=True)
+        subprocess.run(f"mkdir -p {home}/.local/bin", shell=True)
+        subprocess.run(f"wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage -O {home}/.local/bin/nvim", shell=True)
     
     elif pm == "pacman":
         packages = "git build-essential htop tree tmux neovim wget curl"
-        subprocess.run(["sudo", pm, "-Syu", packages])
+        subprocess.run(f"sudo {pm} -Syu packages", shell=True)
 
 def install_config():
     # make config dir
-    subprocess.run(["mkdir", "-p", f"{home}/.config/"])
+    subprocess.run([f"mkdir -p {home}/.config/", shell=True])
 
     # copy stuff
     for x in glob.glob('./.config/*'):
-        subprocess.run(["cp", "-r", x, f"{home}/.config"])
+        subprocess.run(f"cp -r {x} {home}/.config", shell=True)
 
     #copy bashrc
-    subprocess.run(["cp", "./.bashrc", f"{home}/"])
+    subprocess.run(f"cp ./.bashrc {home}/", shell=True)
 
 print("What is your package manager: ")
 print("[1]  apt")
